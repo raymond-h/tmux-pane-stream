fd20289b6bb7d6cdc9d7868121776dcafa8143ff
Raymond Hammarling <raykay.rh@gmail.com>
2015-09-13 01:28:28 +0200

    Add readable part of pane stream

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// The entry point of all to come...
+import { spawn } from 'child_process';
+import stream from 'stream';
+
+export class TmuxPaneWritable extends stream.Writable {
+    constructor(targetPane) {
+        super();
+        this.targetPane = targetPane;
+    }
+
+    _write(chunk, encoding, done) {
+        chunk = chunk.toString();
+
+        const cp = spawn('tmux', ['send-keys', '-l', '-t', this.targetPane, chunk]);
+
+        cp
+        .once('exit', (code) => {
+            if(code === 0) done();
+        })
+        .once('error', done);
+    }
+}
